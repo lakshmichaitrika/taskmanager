34e0ca117b1319e8b867a28f994de1de176799ac
chaitrika <lakshmichaitrika2002@gmail.com>
2024-01-29 13:14:40 +0530

    cha

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Header from './components/header/Header';
 import TaskManager from './components/index'
 import './App.css';
-
+import { CartProvider } from 'react-use-cart';
 
 function App() {
 	
 	return ( 
-		<div>
+		<>
+		<CartProvider>
 			<Header/>
 		<TaskManager/>
-		</div>
+		</CartProvider>
+		</>
 	);
 }
 
